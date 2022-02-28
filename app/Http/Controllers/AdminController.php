238453295c615a949ae3e13f5734eff75d295473
chantyTUN <cambodia4khmeru@gmail.com>
2022-02-28 12:01:53 +0700

    4-Admin Login & Logout Guard Part 1

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -7,6 +7,10 @@ use Illuminate\Http\Request;
 class AdminController extends Controller
 {
     public function Index(){
-        return view();
+        return view('admin.admin_login');
+    } // end method
+
+    public function Dashboard(){
+        return view('admin.index');
     } // end method
 }
